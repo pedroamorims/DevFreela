d80c478152b34cf2767586e056e56a0c7eff1710
pedroamorims <pedrohenrique.as18@gmail.com>
2024-05-21 21:59:35 -0300

    Remover arquivos do cache

--- FreelApp.Application/Queries/GetAllSkills/GetAllSkillsQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using FreelApp.Application.ViewModels;
-using MediatR;
-
-namespace FreelApp.Application.Queries.GetAllSkills
-{
-    public class GetAllSkillsQuery : IRequest<List<SkillViewModel>>
-    {
-    }
-}
